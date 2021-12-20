166f1cb2e2fa4c0bebb2aa329852d5d0fa39fe77
EC2 Default User <ec2-user@ip-172-31-10-116.us-east-2.compute.internal>
2021-12-20 10:16:55 +0000

    first commit

---
+++ src/main/java/com/infosys/App.java
@@ -0,0 +1,13 @@
+package com.infosys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a infosys company, doing in NAB project!" );
+    }
+}
